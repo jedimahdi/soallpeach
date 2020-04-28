840aee936c640d2854b5b3e7d457d24d2dc3fb55
Mahdi SE <seyedmahdiseydan78@yahoo.com>
2020-04-28 17:22:58 +0430

    countme: using native http server instead of express

--- countme/src/index.ts
+++ countme/src/index.ts
@@ -1,35 +1,38 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+import http from 'http'
 
-const app = express()
+const sendResponse = (
+  response: any,
+  data: string,
+  statusCode: number,
+  headers: any,
+) => {
+  response.writeHead(statusCode, headers)
+  response.end(data)
+}
+
+const collectData = (request: any, callback: any) => {
+  let data = ''
+  request.on('data', (chunk: any) => {
+    data += chunk
+  })
+  request.on('end', () => {
+    callback(data)
+  })
+}
 
 let sum = 0
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+const server = http.createServer((req, res) => {
+  const url = req.url
 
-app.post('/', (req, res) => {
-  let newNumberStr = '0'
-  if (typeof req.body === 'string') {
-    newNumberStr = req.body
+  if (url === '/') {
+    collectData(req, (data: any) => {
+      sum += parseInt(data, 10)
+      sendResponse(res, data, 200, {'Content-Type': 'text/plain'})
+    })
+  } else if (url === '/count') {
+    sendResponse(res, `${sum}`, 200, {'Content-Type': 'text/plain'})
   }
-
-  if (typeof req.body === 'object') {
-    newNumberStr = Object.keys(req.body)[0]
-  }
-
-  const n = parseInt(newNumberStr, 10)
-
-  if (isNaN(n)) {
-    return res.sendStatus(400)
-  }
-  sum += n
-
-  res.sendStatus(200)
 })
 
-app.get('/count', (req, res) => {
-  res.status(200).send(`${sum}`)
-})
-
-app.listen(80, () => {})
+server.listen(80)
